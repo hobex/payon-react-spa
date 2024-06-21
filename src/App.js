ca568d7b367b4b3639a3d1dbae6148ae44c3d676
Thomas Tisch <thomas.tisch@hobex.at>
2024-06-21 11:25:00 +0200

    Change checkout ids

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 //
 export default function App() {
   const [checkoutId, setCheckoutId] = useState(
-    "E256E2E83931796CE090D19D9B94C2DE.uat01-vm-tx01" // use your own checkoutId
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx.uat01-vm-tx01" // use your own checkoutId
   );
   return (
     <>
       <button
         onClick={() =>
-          setCheckoutId("7566A73ADF7D5C8C5ECDD019DE328889.uat01-vm-tx02") // use your own 2nd checkoutId
+          setCheckoutId("xxxxxxxxxxxxxxxxxxxxxxxxxxx.uat01-vm-tx02") // use your own 2nd checkoutId
         }
       >
         Renew Checkout
